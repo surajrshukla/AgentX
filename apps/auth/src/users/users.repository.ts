0b3587f548c8577325c6e640522ff08cbb41646f
Suraj Shukla <suraj@zengroup.co.in>
2024-07-11 19:06:26 +0530

    [REF] - refectored and switched from npm to pnpm

--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,12 +1,11 @@
-import { Abstractrepository } from '@app/common';
+import { Abstractrepository, UserDocument } from '@app/common';
 import { Injectable, Logger } from '@nestjs/common';
-import { UserDocument } from '@app/common/models';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersRepository extends Abstractrepository<UserDocument> {
-    protected readonly logger = new Logger(UsersRepository.name);
+    protected readonly logger = new Logger(UserDocument.name);
 
     constructor(
         @InjectModel(UserDocument.name) userModel: Model<UserDocument>,
